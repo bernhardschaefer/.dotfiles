d77617def6f3de6e2206c0125e6c458930893b49
Bernhard Schäfer <bschaefer87@gmx.net>
2014-08-13 09:25:13 +0200

    set caps as escape without switching

--- linux-startup.sh
+++ linux-startup.sh
@@ -11,17 +11,8 @@
 # setxkbmap -option caps:swapescape
 
 # dconf setting seems to be more reliable
-dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:swapescape']"
-
-# --- Switch between keyboards using alt+shift ---
-# (ubuntu 13.10 text entry settings are buggy)
-
-# direct way:
-# setxkbmap -option grp:alt_shift_toggle us,de
-
-# ppa fix 'Modifier-only input switch' 
-# http://askubuntu.com/questions/356357/how-to-use-altshift-to-switch-keyboard-layouts-in-13-10
-
+# dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:swapescape']"
+dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
 
 # verify correct settings
 # setxkbmap -query
