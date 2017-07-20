2fc8cd8e2ecef73e87b52746dcb82051dc5d188b
Bernhard Schäfer <bernhard.schaefer1@gmail.com>
2017-07-20 10:33:05 +0200

    sourcing .vimrc for nvim

--- setup.sh
+++ setup.sh
@@ -3,3 +3,12 @@
 DIR=~/ws/git/dotfiles
 ln -vs $DIR/bashrc ~/.bashrc 
 ln -vs $DIR/vimrc ~/.vimrc 
+
+mkdir -p ~/.config/nvim/
+
+cat > ~/.config/nvim/init.vim << EOL
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath = &runtimepath
+source ~/.vimrc
+EOL
+
