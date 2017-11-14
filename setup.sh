8b2a879f8fa1a90419f7b3777f6a230f14ff82ab
Bernhard Schäfer <bernhard.schaefer1@gmail.com>
2017-11-14 21:26:34 +0100

    term profile fix for italics in vim markdown

--- setup.sh
+++ setup.sh
@@ -4,8 +4,11 @@ DIR=~/ws/git/dotfiles
 ln -vs $DIR/bashrc ~/.bashrc 
 ln -vs $DIR/vimrc ~/.vimrc 
 
-mkdir -p ~/.config/nvim/
+# markdown italics
+tic xterm-256color-italic.terminfo
 
+# NVIM
+mkdir -p ~/.config/nvim/
 cat > ~/.config/nvim/init.vim << EOL
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
